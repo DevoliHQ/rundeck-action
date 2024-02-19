5089a3ed56c9cac43d9856db2469d0c634b598fc
tbotnz <tonynealon1989@gmail.com>
2024-02-19 23:31:42 +0000

    add simple retry

--- app/command/rundeck.py
+++ app/command/rundeck.py
@@ -6,6 +6,7 @@ import click
 import json
 import time
 from pyrundeck import Rundeck
+from tenacity import retry, stop_after_attempt
 
 
 @click.group()
@@ -28,18 +29,20 @@ def cli():
               default="true")
 @click.option("--job-id",
               help="Rundeck job ID.",
-              required = True)
+              required=True)
 @click.option("--options",
               help="Rundeck job options in json format.")
 @click.option("--wait",
               help="Wait Rundeck job finish and print output.")
+@retry(stop=stop_after_attempt(3))
 def execute_rundeck_job(rundeck_url, rundeck_token, api_version, verify_ssl, job_id, options=None, wait=None):
     """Exetute Rundeck job and get output."""
 
     if options:
         options = json.loads(options)
 
-    rundeck = Rundeck(rundeck_url, token=rundeck_token, api_version=api_version, verify=(verify_ssl == 'true'))
+    rundeck = Rundeck(rundeck_url, token=rundeck_token,
+                      api_version=api_version, verify=(verify_ssl == 'true'))
 
     run = rundeck.run_job(job_id, options=options)
     print("Scheduled Rundeck job: %d" % run['id'])
@@ -53,8 +56,8 @@ def execute_rundeck_job(rundeck_url, rundeck_token, api_version, verify_ssl, job
             time.sleep(5)
 
         for log_entry in rundeck.execution_output_by_id(run['id'])['entries']:
-            print("%s %s %s" % (log_entry['absolute_time'], log_entry['level'], log_entry['log']))
+            print("%s %s %s" % (
+                log_entry['absolute_time'], log_entry['level'], log_entry['log']))
 
         if rundeck.execution_state(run['id'])['executionState'] != 'SUCCEEDED':
             exit(1)
-
